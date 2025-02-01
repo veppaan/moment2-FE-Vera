c4db1875e3870069968c5d80c4e0676809a00431
Vera Kippel <vera.kippel@outlook.com>
2025-02-01 18:21:12 +0100

    Skapar en tabell

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -605,10 +605,12 @@ window.onload = ()=>{
 async function loadCourses() {
     try {
         const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+        if (!response.ok) throw new Error("Fel vid ladding av data...");
         const data = await response.json();
         console.table(data);
     } catch (error) {
         console.error(error);
+        document.querySelector("#error").innerHTML = "<p>Ett fel uppstod - prova igen senare!</p>";
     }
 }
 
